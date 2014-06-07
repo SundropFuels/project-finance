0ac96b55898274b89bb1337eda3401b6b7bb401c
Chris Perkins <chris@aegis.(none)>
2014-06-07 14:56:55 -0600

    Added arithmetic functions (left and right) to UnitVal

--- UnitValues.py
+++ UnitValues.py
@@ -63,4 +63,77 @@ class UnitVal:
     def __mul__(self, other):
         """Returns the product of this with other, in this' units, as a UnitVal"""
         #Want to include the ability to do scalar multiplication as well as uv multiplication
-        pass
+        if isinstance(other, UnitVal):
+            conv = uc.UnitConverter()
+            try:
+                rarg = conv.convert(other.value, other.units, self.units)
+                return UnitVal(self.value * rarg, self.units)
+            except uc.InconsistentUnitError:
+                raise ValueError, "Inconsistent units, %s, %s" % (other.units, self.units)
+
+        else:
+            #Need to verify that other is numeric by doing something you should only be able to do to numbers
+            try:
+                a = other * 2.3544
+            except ValueError:
+                raise ValueError, "UnitVal can only be multiplied by UnitVal or a scalar"
+            return UnitVal(self.value * other, self.units)
+
+    def __div__(self, other):
+        """Returns the quotient of this by other, in this' units, as a UnitVal"""
+        if isinstance(other, UnitVal): 
+            conv = uc.UnitConverter()
+            try:
+                divisor = conv.convert(other.value, other.units, self.units)
+                if divisor == 0:
+                    raise ValueError, "Divide by zero error"
+                return UnitVal(self.value/divisor, self.units)
+            except uc.InconsistentUnitError:
+                raise ValueError, "Inconsistent units, %s, %s" % (other.units, self.units)
+	else:
+            try:
+                a = other * 2.3544
+            except ValueError:
+                raise ValueError, "UnitVal can only be divided by UnitVal or a scalar"
+            if other == 0:
+                raise ValueError, "Divide by zero error"  #May want to make this np.inf instead
+            return UnitVal(self.value/other, self.units)
+
+    def __rmul__(self, other):
+        """Returns the product of this with other, in others' units (if applicable), as a UnitVal"""
+        #Want to include the ability to do scalar multiplication as well as uv multiplication
+        if isinstance(other, UnitVal):
+            conv = uc.UnitConverter()
+            try:
+                rarg = conv.convert(self.value, self.units, other.units)
+                return UnitVal(other.value * rarg, other.units)
+            except uc.InconsistentUnitError:
+                raise ValueError, "Inconsistent units, %s, %s" % (other.units, self.units)
+
+        else:
+            #Need to verify that other is numeric by doing something you should only be able to do to numbers
+            try:
+                a = other * 2.3544
+            except ValueError:
+                raise ValueError, "UnitVal can only be multiplied by UnitVal or a scalar"
+            return UnitVal(self.value * other, self.units)
+
+    def __rdiv__(self, other):
+        """Returns the quotient of this by other, in this' units, as a UnitVal"""
+        if isinstance(other, UnitVal): 
+            conv = uc.UnitConverter()
+            try:
+                divisor = conv.convert(self.value, self.units, other.units)
+                if divisor == 0:
+                    raise ValueError, "Divide by zero error"
+                return UnitVal(other.value/divisor, other.units)
+            except uc.InconsistentUnitError:
+                raise ValueError, "Inconsistent units, %s, %s" % (other.units, self.units)
+	else:
+            try:
+                a = other * 2.3544
+            except ValueError:
+                raise ValueError, "UnitVal can only be divided by UnitVal or a scalar"
+            if self.value == 0:
+                raise ValueError, "Divide by zero error"  #May want to make this np.inf instead
+            return UnitVal(other/self.value, self.units)

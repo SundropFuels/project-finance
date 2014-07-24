Fixed the daily percentage rate calculation in the inflation excalator (and corresponding test) to np.power(1+rate...) vs np.power(rate...)

ProjectFinance_daily.py
@@ -571,27 +571,28 @@ class Escalator:
 
         return self.factor * cost
 
-class NoEscalationEscalator:
+class NoEscalationEscalator(Escalator):
     """Scales to a constant value"""
     def escalate(self, **kwargs):
         self.factor = 1.0
-        Escalator.escalate(self, **kwargs)    
+        return Escalator.escalate(self, **kwargs)    
 
 
-class InflationRateEscalator:
+class InflationRateEscalator(Escalator):
     """Uses a fixed inflation rate (annual, effective) to determine the final cost"""
     def escalate(self, rate, **kwargs):
         try:
-            dpr = (np.power(rate, 1/365.0)-1)
-            n = (kwargs[new_date]-kwargs[basis_date]).days	#number of days between intervening periods
-            self.factor = (1+rate)**n
-            Escalator.escalate(self, **kwargs)
+            dpr = np.power(1+rate, 1/365.0)-1
+            n = (kwargs['new_date']-kwargs['basis_date']).days	#number of days between intervening periods
+            print n
+            self.factor = (1+dpr)**n
+            return Escalator.escalate(self, **kwargs)
         except KeyError:
             raise MissingInfoError, "We are missing some data from the escalate function"
         except ValueError:
             raise ProjFinError, "The values used for the inflation rate are invalid"
 
-class CPIindexEscalator:
+class CPIindexEscalator(Escalator):
     """Uses the CPI index to determine the final cost"""
     #Need a pandas timeseries with values of the CPI index hardcoded
 

capex_tests.py
@@ -33,7 +33,7 @@ class CapitalExpenseTests(unittest.TestCase):
         """Not passing in proper dates should throw an error"""
         start = 'poop'
 	finish = dt.datetime(2012,01,01)
-        es = NoEscalationEscalator()
+        es = pf.NoEscalationEscalator()
         self.assertRaises(pf.BadDateError, es.escalate, 100.0, start, finish)
         start = finish
         finish = 234.0
@@ -43,7 +43,7 @@ class CapitalExpenseTests(unittest.TestCase):
         """Cost should be a non-zero value"""
 	start = dt.datetime(2010,01,01)
         finish = dt.datetime(2012,01,01)
-        es = NoEscalationEscalator()
+        es = pf.NoEscalationEscalator()
         self.assertRaises(pf.BadValue, es.escalate, 'moop', start, finish)
 
         
@@ -51,15 +51,15 @@ class CapitalExpenseTests(unittest.TestCase):
         """Test whether an inflation rate is properly escalated"""
         start = dt.datetime(2010,01,01)
         finish = dt.datetime(2011,01,01)
-        es = InflationRateEscalator()
+        es = pf.InflationRateEscalator()
         val = es.escalate(rate = 0.015, cost = 100.0, basis_date = start, new_date = finish)
-        actual = 100.0*(1+rate)
-        self.assertEqual(val,actual)
+        actual = 100.0*(1+0.015)
+        self.assertAlmostEqual(val,actual)
         finish = dt.datetime(2012,06,25)
         val = es.escalate(rate = 0.015, cost = 100.0, basis_date = start, new_date = finish)
-        r = (np.power(rate,1/365.0)-1)
+        r = (np.power(1.015,1/365.0)-1)
         actual = 100.0 * (1+r)**906
-        self.assertEqual(val, actual)
+        self.assertAlmostEqual(val, actual)
 
     def testBadEscalationRate(self):
         """Rate should be non-zero, or throw an error"""
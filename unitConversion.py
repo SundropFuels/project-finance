Started to expand UnitValues to make multiplication of unit values correct.  Added a unit simplification tool to the unit conversion library that, if I may so myself, kicks ass.

unitConversion.py
@@ -1,6 +1,8 @@
 """Unit conversion module for Python"""
 
 import numpy as np
+import itertools as it
+import copy
 
 class UnitError(Exception):
     pass
@@ -29,20 +31,24 @@ class UnitConverter:
 
     energy_dict = {'J':1.0, 'kJ':0.001, 'MJ':1E-6, 'cal':0.238845896628, 'kcal':238.845896628, 'Btu':9.47817120313E-4, 'MMBtu':9.47817120313E-10}
     energy_units = 'kg*m^2/s^2'    
+    energy_pref_u = 'J'
 
     pressure_dict = {'Pa':1,'psi':1.4503773773E-4, 'bar':1E-5, 'atm':9.86923266716E-6, 'mmHg':0.007500616827, 'mmH2O':0.101971621298, 'inH2O':0.00401463076}
     pressure_units = 'kg/s^2/m'
+    pressure_pref_u = 'Pa'
 
     volume_dict = {'L':1000.0, 'gal':264.172052358}
     volume_units = 'm^3'
+    volume_pref_u = 'L'
 
     power_dict = {'W':1, 'kW':0.001, 'MW':1E-6, 'mW':1000}
     power_units = 'kg*m^2/s^3'
+    power_pref_u = 'W'
 
 
     units = [mass_dict, time_dict, length_dict, mole_dict, temperature_dict, money_dict]
     derived_units = [(energy_units, energy_dict),(pressure_units,pressure_dict),(volume_units,volume_dict),(power_units,power_dict)]
-
+    derived_preferred_units = [energy_pref_u, pressure_pref_u, volume_pref_u, power_pref_u]
 
     non_abs_temp_factors = {'K':0, 'C':-273.15, 'R':0, 'F':-459.67}
 
@@ -53,6 +59,184 @@ class UnitConverter:
     def __init__(self):
         self.unit_list = []
 
+
+    def simplify_units(self, unit):
+	"""Returns a reduced unit string for the given unit"""
+        parsed = self._parse_inputstr(unit)
+	#Now I have a list of (unit, exponent) tuples
+
+	#Strategy1: 
+	#Parse the string
+	#Determine the preferred units?  maybe?
+	#Shift the derived units to base units, tack onto the list
+	#File the list into base unit buckets
+	#Add the exponents
+	#Search for derived unit permutations among the derived unit set, rewrite in terms of those
+	#Recast in terms of preferred units
+	#Calculate the return factor
+
+
+        
+
+	if len(parsed) == 1:
+            return unit
+
+	simplified_list = [0] * len(UnitConverter.units)
+	self.unit_names = [None] * len(UnitConverter.units)
+	dunit_names = copy.deepcopy(UnitConverter.derived_preferred_units)
+	found = False
+	for (u, exponent) in parsed:
+                    
+
+            for (base_units, unit_dict) in UnitConverter.derived_units:
+                for (key,value) in unit_dict.items():
+                    if u == key:
+                        found = True
+                        dunit_names[UnitConverter.derived_units.index((base_units,unit_dict))] = u	#uses the first derived unit it encounters as the preferred unit
+                        parsed_sub = self._parse_inputstr(base_units)
+                        for st, exponent2 in parsed_sub:
+                            parsed.append((st, float(exponent2)*float(exponent)))
+
+                if found == True:
+                    break
+
+            
+            for unit_dict in UnitConverter.units:
+                for (key, value) in unit_dict.items():
+                    if u == key:
+                        
+                        found = True
+                        simplified_list[UnitConverter.units.index(unit_dict)] += exponent
+			if self.unit_names[UnitConverter.units.index(unit_dict)] is None:
+			    self.unit_names[UnitConverter.units.index(unit_dict)] = u		#uses the first base unit it encounters as the natural unit -- can do the same above for derived units
+
+
+                        break
+                if found == True:
+                    break
+
+            if found == False:
+                raise UnitNotFoundError, "The unit %s is not in the database" % u
+            found = False
+
+	found = False
+
+	#Now we should have a list of the net exponents; it should be easy to write a new string for this
+	out_string = ""
+	for unit_dict in UnitConverter.units:
+	    if simplified_list[UnitConverter.units.index(unit_dict)] != 0:
+	        out_string += "%s^%s*" % (self.unit_names[UnitConverter.units.index(unit_dict)], simplified_list[UnitConverter.units.index(unit_dict)])
+        out_string = out_string[:-1]
+	#right now, this is where I'm stopping -- I can search for derived units when I have this working
+
+	#ok -- now we want to look for derived units
+	#the simplified_list contains [[exponent1],[exponent2],[exponent3],...] for the base units involved here
+	#if we iterate over the absolute value of the exponents, starting at the highest value from the first base unit, we can cover all combinations
+	#The combinations here are given by the itertools.combinations library, which we want to sort from longest to shortest
+	#We'll create all of the combinations, then sort them by the sum of their exponents.  That is what we'll do.
+	#[[4,1,0], [3,1,0], [2,1,0], [1,1,0], [4,0,0], ...]
+	
+        drv_list = [0] * len(UnitConverter.derived_units)
+        (simplified_list, derived_list) = self._find_derived_units(simplified_list, drv_list)
+
+
+        out_string = ""
+        for unit_dict in UnitConverter.units:
+            if simplified_list[UnitConverter.units.index(unit_dict)] != 0:
+                out_string += "%s^%s*" % (self.unit_names[UnitConverter.units.index(unit_dict)], simplified_list[UnitConverter.units.index(unit_dict)])
+
+        for (base_unit, unit_dict) in UnitConverter.derived_units:
+            if derived_list[UnitConverter.derived_units.index((base_unit,unit_dict))] != 0:
+                out_string += "%s^%s*" % (dunit_names[UnitConverter.derived_units.index((base_unit,unit_dict))], derived_list[UnitConverter.derived_units.index((base_unit,unit_dict))])
+        out_string = out_string[:-1]
+
+        
+	return out_string
+
+    def _find_derived_units(self, smp_list, drv_list):
+        """Determines whether a simplified unit expression in base units can be expressed in more complicated derived units.
+           Searches the most complex units first.  smp_list is a list of exponents for base units in the simplified expression,
+           ordered by the unit dictionary at the top.  Ordering is implicit.  drv_list is the list of exponents for derived
+           units in the new expression, ordered by its unit dictionary at the top, with implicit ordering.
+           Returns (smp_list,drv_list) tuple, with a new simplified list and derived list)"""
+        lsl = self._simp_list(smp_list)		#this will have the simplified list, now we need to organize it by exponential sum
+        lsl_d = {lsl.index(lsl_i):sum(lsl_i) for lsl_i in lsl}
+	lsl_s = sorted(lsl_d, key = lsl_d.get)		#this is the sorted list of indices in simplified_list -- we want to work it in reverse order
+        N = len(lsl_s) - 1
+        
+	while N >0:
+            #build the unit string
+            u_string = ""
+	    temp_list = lsl[lsl_s[N]]
+
+	    for unit_dict in UnitConverter.units:
+                i = UnitConverter.units.index(unit_dict)
+                if temp_list[i] != 0:
+                    u_string += "%s^%s*" % (self.unit_names[i], int(temp_list[i]*np.sign(smp_list[i])))
+            u_string = u_string[:-1]			#this should be the simplified unit string, now we'll test it against all of the base units in the derived dictionary
+            N -= 1
+	    
+	    
+	    for bu, dd in UnitConverter.derived_units:
+                try:
+                    self.convert_units(1.0, u_string, bu)	#this attempts to convert between the base unit in the derived dictionary and the subunit string
+                    #now strip out the derived unit from the simplified_list and tag the derived unit onto the new parsed list -- easier said than done
+		    ###!!!###
+                    #increment the derived unit by one
+                    drv_list[UnitConverter.derived_units.index((bu,dd))] += 1
+		    #parse the string to get the exponents
+                    dec = self._parse_inputstr(u_string)
+                    for (u,exponent) in dec:
+                        for unit_dict in UnitConverter.units:
+                            if u in unit_dict:
+                                smp_list[UnitConverter.units.index(unit_dict)] -= exponent
+                                break
+		                #return the list with the function called on itself with the shorter list
+                            #this should be safe -- base units should have already been vetted if I get here, so u should not NOT be in a unit_dict
+                    return self._find_derived_units(smp_list, drv_list)
+                except InconsistentUnitError:
+                    #flip it around
+                    try:
+                        self.convert_units(1.0, u_string, "(%s)^-1" % bu)
+                        drv_list[UnitConverter.derived_units.index((bu,dd))] -= 1
+                        dec = self._parse_inputstr(u_string)
+                        for (u,exponent) in dec:
+                            for unit_dict in UnitConverter.units:
+                                if u in unit_dict:
+                                    smp_list[UnitConverter.units.index(unit_dict)] -= exponent
+                                    break
+                        return self._find_derived_units(smp_list,drv_list)
+
+                    except InconsistentUnitError:
+                        pass
+        
+            #didn't find any derived units.  Just return the simplified list and the derived unit list
+
+        
+        return (smp_list, drv_list)    
+
+    def _simp_list(self, ll):
+        rlist = []
+
+
+        #need a base case here
+
+        if len(ll) == 1:
+            for j in range(0,int(abs(ll[0]))+1):
+                rlist.append([j])
+            return rlist
+    
+        #iterating case
+        
+        for i in range(0, int(abs(ll[0]))+1):       #if this were zero, it would skip it
+            sl = self._simp_list(ll[1:])		#want this to return a list of lists, with exponents at the appropriate location
+            for l in sl:
+                l.insert(0,i)
+            rlist.extend(sl)
+        
+        return rlist
+
+        
     
     def _derived_unit_replace(self, unit):
         """Takes derived units (N, J, etc.) and replaces them with new text representing the fundamental units"""
@@ -162,7 +346,7 @@ class UnitConverter:
             if checksum != 0:
                 raise InconsistentUnitError, "The to and from units do not match"
 
-        #print consistency_list
+
         
         return val * factor
 